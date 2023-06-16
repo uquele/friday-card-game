ef8d4d36236804220d10e64a398602241c6b4d72
Nikita Maltsev <nisa4414@gmail.com>
2023-06-16 06:05:32 -0700

    Added initial setup. Not tested. To do: fighting mechanics

--- cards-tsv.js
+++ cards-tsv.js
@@ -1,70 +1,70 @@
-const CARDS_TSV = `type	name	power	skillName	removeCost	dangerStage1	dangerStage2	dangerStage3	draw	agingType	agingEffectName	pirateEffectName
-Robinson	Weak	-1		1							
-Robinson	Hungry	-1		1							
-Robinson	Scared	-1		1							
-Robinson	Confused	-1		1							
-Robinson	Sleepy	-1		1							
-Robinson	Well fed	0	Life +2	1							
-Robinson	Stupid	0		1							
-Robinson	Lazy	0		1							
-Robinson	Depressed	0		1							
-Robinson	Curious	0		1							
-Robinson	Dumb	0		1							
-Robinson	Naive	0		1							
-Robinson	Foolish	0		1							
-Robinson	Slow	0		1							
-Robinson	Crafty	1		1							
-Robinson	Happy	1		1							
-Robinson	Courageous	1		1							
-Robinson	Strong	2		1							
-Danger	Cannibals	4		1	5	9	14	5			
-Danger	Wild bear	4		1	5	9	14	5			
-Danger	Anaconda	3	Destroy	1	4	7	11	4			
-Danger	Wild boar	3	Draw +1	1	4	7	11	4			
-Danger	Wild wolf	3	Vision	1	4	7	11	4			
-Danger	Jaguar	3	Exchange 1	1	4	7	11	4			
-Danger	Earthquake	2	Exchange 1	1	2	5	8	3			
-Danger	Flood	2	Double	1	2	5	8	3			
-Danger	Storm	2	Draw +1	1	2	5	8	3			
-Danger	Cobra	2	Destroy	1	2	5	8	3			
-Danger	Quicksand	2	Vision	1	2	5	8	3			
-Danger	Hurricane	2	Life +1	1	2	5	8	3			
-Danger	Landslide	1	Life +1	1	1	3	6	2			
-Danger	Hail	2		1	1	3	6	2			
-Danger	Wild pig	1	Double	1	1	3	6	2			
-Danger	Tarantula	1	Life +1	1	1	3	6	2			
-Danger	Wasp nest	1	Copy	1	1	3	6	2			
-Danger	Heavy rain	1	Destroy	1	1	3	6	2			
-Danger	Piranhas	1	Put under pile	1	1	3	6	2			
-Danger	Flu	2		1	1	3	6	2			
-Danger	Deep cave	0	Put under pile	1	0	1	3	1			
-Danger	Wild horses	0	Copy	1	0	1	3	1			
-Danger	Heat	0	Draw +2	1	0	1	3	1			
-Danger	Swift river	0	Draw +2	1	0	1	3	1			
-Danger	Raft	0	Life +1	1	0	1	3	1			
-Danger	Rocky shore	0	Stage -1	1	0	1	3	1			
-Danger	Scarce food	0	Destroy	1	0	1	3	1			
-Danger	Steep slope	0	Life +1	1	0	1	3	1			
-Danger	Mosquitoes	0	Exchange 2	1	0	1	3	1			
-Danger	Closed chest	0	Exchange 2	1	0	1	3	1			
-Aging	Nauseous	0		2					Old	Highest 0	
-Aging	Wounded	-2		2					Old		
-Aging	Exhausted	0		2					Old	Stop	
-Aging	Very weak	-1		2					Old		
-Aging	Sick	0		2					Old	Life -1	
-Aging	Very stupid	-3		2					Old		
-Aging	Terrified	-2		2					Old		
-Aging	Dizzy	0		2					Old	Highest 0	
-Aging	Suicidal	-5		2					Very old		
-Aging	Very old	-4		2					Very old		
-Aging	Very sick	0		2					Very old	Life -2	
-Pirates	6 pirates	20						6			none
-Pirates	7 pirates	25						7			none
-Pirates	8 pirates	30						8			none
-Pirates	9 pirates	35						9			none
-Pirates	10 pirates	40						10			none
-Pirates	Scary pirates	22						9			Only half of your cards (round up) have any power
-Pirates	Slow pirates	52						10			Each of your cards has +1 power
-Pirates	Archers	16						7			Each extra card drawn costs 2 life instead of 1
-Pirates	Swordsmen	X						5			Power X is number of aging cards added to game x2
-Pirates	Pack of hounds	X						X			You have to beat all unbeaten danger cards at once`
+const CARDS_TSV = `id	type	name	power	skillName	removeCost	phaseGreen	phaseYellow	phaseRed	draw	agingType	agingEffectName	pirateEffectName
+1	Fighting	Weak	-1		1							
+2	Fighting	Hungry	-1		1							
+3	Fighting	Scared	-1		1							
+4	Fighting	Confused	-1		1							
+5	Fighting	Sleepy	-1		1							
+6	Fighting	Well fed	0	Life +2	1							
+7	Fighting	Stupid	0		1							
+8	Fighting	Lazy	0		1							
+9	Fighting	Depressed	0		1							
+10	Fighting	Curious	0		1							
+11	Fighting	Dumb	0		1							
+12	Fighting	Naive	0		1							
+13	Fighting	Foolish	0		1							
+14	Fighting	Slow	0		1							
+15	Fighting	Crafty	1		1							
+16	Fighting	Happy	1		1							
+17	Fighting	Courageous	1		1							
+18	Fighting	Strong	2		1							
+19	Hazard	Cannibals	4		1	5	9	14	5			
+20	Hazard	Wild bear	4		1	5	9	14	5			
+21	Hazard	Anaconda	3	Destroy	1	4	7	11	4			
+22	Hazard	Wild boar	3	Draw +1	1	4	7	11	4			
+23	Hazard	Wild wolf	3	Vision	1	4	7	11	4			
+24	Hazard	Jaguar	3	Exchange 1	1	4	7	11	4			
+25	Hazard	Earthquake	2	Exchange 1	1	2	5	8	3			
+26	Hazard	Flood	2	Double	1	2	5	8	3			
+27	Hazard	Storm	2	Draw +1	1	2	5	8	3			
+28	Hazard	Cobra	2	Destroy	1	2	5	8	3			
+29	Hazard	Quicksand	2	Vision	1	2	5	8	3			
+30	Hazard	Hurricane	2	Life +1	1	2	5	8	3			
+31	Hazard	Landslide	1	Life +1	1	1	3	6	2			
+32	Hazard	Hail	2		1	1	3	6	2			
+33	Hazard	Wild pig	1	Double	1	1	3	6	2			
+34	Hazard	Tarantula	1	Life +1	1	1	3	6	2			
+35	Hazard	Wasp nest	1	Copy	1	1	3	6	2			
+36	Hazard	Heavy rain	1	Destroy	1	1	3	6	2			
+37	Hazard	Piranhas	1	Put under pile	1	1	3	6	2			
+38	Hazard	Flu	2		1	1	3	6	2			
+39	Hazard	Deep cave	0	Put under pile	1	0	1	3	1			
+40	Hazard	Wild horses	0	Copy	1	0	1	3	1			
+41	Hazard	Heat	0	Draw +2	1	0	1	3	1			
+42	Hazard	Swift river	0	Draw +2	1	0	1	3	1			
+43	Hazard	Raft	0	Life +1	1	0	1	3	1			
+44	Hazard	Rocky shore	0	Stage -1	1	0	1	3	1			
+45	Hazard	Scarce food	0	Destroy	1	0	1	3	1			
+46	Hazard	Steep slope	0	Life +1	1	0	1	3	1			
+47	Hazard	Mosquitoes	0	Exchange 2	1	0	1	3	1			
+48	Hazard	Closed chest	0	Exchange 2	1	0	1	3	1			
+49	Aging	Nauseous	0		2					Old	Highest 0	
+50	Aging	Wounded	-2		2					Old		
+51	Aging	Exhausted	0		2					Old	Stop	
+52	Aging	Very weak	-1		2					Old		
+53	Aging	Sick	0		2					Old	Life -1	
+54	Aging	Very stupid	-3		2					Old		
+55	Aging	Terrified	-2		2					Old		
+56	Aging	Dizzy	0		2					Old	Highest 0	
+57	Aging	Suicidal	-5		2					Very old		
+58	Aging	Very old	-4		2					Very old		
+59	Aging	Very sick	0		2					Very old	Life -2	
+60	Pirates	6 pirates	20						6			none
+61	Pirates	7 pirates	25						7			none
+62	Pirates	8 pirates	30						8			none
+63	Pirates	9 pirates	35						9			none
+64	Pirates	10 pirates	40						10			none
+65	Pirates	Scary pirates	22						9			Only half of your cards (round up) have any power
+66	Pirates	Slow pirates	52						10			Each of your cards has +1 power
+67	Pirates	Archers	16						7			Each extra card drawn costs 2 life instead of 1
+68	Pirates	Swordsmen	X						5			Power X is number of aging cards added to game x2
+69	Pirates	Pack of hounds	X						X			You have to beat all unbeaten danger cards at once`
--- game.js
+++ game.js
@@ -2,33 +2,136 @@
 //@ts-check
 
 const cards = parseTSV(CARDS_TSV)
+// console.table(cards)
 
-console.table(cards)
+
+// INITIAL SETUP
+
+const deckFighting = cards.filter(card => card.type === 'Fighting')
+const deckFightingDiscard = []
+const deckHazard = cards.filter(card => card.type === 'Hazard')
+const deckHazardDiscard = []
+const deckAging = cards.filter(card => card.type === 'Aging')
+const deckPirates = cards.filter(card => card.type === 'Pirates')
+
+const currentHazard = []
+const deckLeftSide = []
+const deckRightSide = []
+
+let difficultyLevel = 1
+let lives = 20
+let livesMax = 22
+
+switch (difficultyLevel) {
+  case 1:
+    removeCard({name: 'Very stupid'}, deckAging)
+    break
+  case 2:
+    removeCard({name: 'Very stupid'}, deckAging)
+    deckFighting.push(drawCard(deckAging, 'random'))
+    break
+  case 3:
+    deckFighting.push(drawCard(deckAging, 'random'))
+    break
+  case 4:
+    lives = 18
+    livesMax = 20
+    deckFighting.push(drawCard(deckAging, 'random'))
+    break
+  default:
+    throw new TypeError(`Difficulty level not found: ${level}`)
+}
+
+shuffle(deckFighting)
+shuffle(deckHazard)
+shuffle(deckAging)
+shuffle(deckPirates)
+
+
 
 
 /**
  * 
- * @param {String} TSVString - first row must contain the names of columns to be used as object keys
- * @returns {Array}
+ * @param {Object} cardQuery - for example, {name: 'Very stupid'}
+ * @param {Array} deck 
+ * @returns {Array} - the removed card
  */
 
-function parseTSV(TSVString) {
-  const rows = TSVString.split('\n')
-  const table = rows.map(row => row.split('\t'))
-  const parsed = []
-
-  for (let rowNumber = 1; rowNumber < table.length; rowNumber++) {
-
-    const obj = {}
-    for (let columnNumber = 0; columnNumber < table[rowNumber].length; columnNumber++) {
-      const key = table[0][columnNumber]
-      const value = table[rowNumber][columnNumber]
-      obj[key] = value
-    }
-    parsed.push(obj)
-
+function removeCard(cardQuery, deck) {  // 
+  const [[key, value]] = Object.entries(cardQuery)
+  const index = deck.findIndex(card => card[key] === value)
+  if (index < 0) {
+    throw new TypeError(`Card not found: {${key}: ${value}}`)
   }
-
-  return parsed
+  const removedCard = deck.splice(index, 1)
+  return removedCard;
 }
 
+
+/**
+ * 
+ * @param {Object} cardQuery - for example, {name: 'Very stupid'}
+ * @param {Array} deckFrom 
+ * @param {Array} deckTo 
+ * @param {String} position - where to insert the card. Either 'bottom' or 'top'
+ * @returns 
+ */
+
+function moveCard(cardQuery, deckFrom, deckTo, position = 'bottom') {
+  let insertIndex
+
+  switch (position) {
+    case 'bottom':
+      insertIndex = 0
+      break
+    case 'top':
+      insertIndex = deckTo.length - 1
+      break
+    default:
+      throw new TypeError(`Position is wrong or not supported. Position: ${position}`)
+  }
+
+  const removedCard = removeCard(cardQuery, deckFrom)
+  deckTo.splice(insertIndex, 0, removedCard)
+  return
+}
+
+/**
+ * 
+ * @param {Array} deck 
+ * @param {String} location - 'top', 'bottom', 'random'
+ * @returns {Object} - card
+ */
+
+function drawCard(deck, location) {
+  let index
+  switch (location) {
+    case 'top':
+      index = deck.length - 1
+      break
+    case 'bottom':
+      index = 0
+      break
+    case 'random':
+      index = randomInt(0, deck.length - 1)
+      break
+    default:
+      throw new TypeError(`Wrong location: ${location}`)
+  }
+
+  const drawnCard = deck.splice(location, 1);
+  return drawnCard;
+
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i >= 0; i--) {
+    let random = randomInt(0, deck.length - 1);
+    [deck[i], deck[random]] = [deck[random], deck[i]]
+  }
+  return;
+}
